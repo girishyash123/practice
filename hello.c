7378c6b0285aca78056b0833fc517f014fd0ed07
girishyash123 <61219873+girishyash123@users.noreply.github.com>
2025-12-30 15:13:13 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,8 +2,9 @@
 
 int main() {
     printf("Hello from Jenkins C build\n");
-    int a=0;
-    int b=1/int(a);
+    int a,b;
+    a = 0;
+    b = 1/a;
     printf("the value of b is %d",b);
     return 0;
 }
